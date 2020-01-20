08f3cd38292d98f6a555f85f55cad95870f6491c
iffy <zyf8395@sina.com>
2020-01-20 10:09:40 +0800

    LeakCanary

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.myapplication
 
+import android.annotation.TargetApi
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.database.Cursor
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.*
+import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -24,14 +33,32 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             produceLeak()
         }
+
     }
 
+    @TargetApi(26)
     fun produceLeak() {
         //AsyncTask持有Activity的引用，导致Leak
         MyTask().execute()
+
         //单例持有ActivityContext导致Leak,应该使用application context
         val s = SingletonT.getSingletonT(this)
+
         wv.loadUrl("https://www.163.com")
+
+        val uri = ContactsContract.Contacts.CONTENT_URI
+        val cursor = contentResolver.query(uri, null, null, null)
+
+
+
+        //registerReceiver(MyBroadCast(),IntentFilter("com.iffy.action"))
+    }
+
+    inner class MyBroadCast : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+        }
+
     }
 
     //内部类默认持有外部类的引用，会导致leak
